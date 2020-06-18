3a56c3641f815fe9269934086bce4f0f87c769b2
lakshay <guptalakshay90@gmail.com>
2020-06-18 15:40:33 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let cron = require('node-cron')
 let shell = require('shelljs')
-
+var express = require('express')
+var app = express()
 cron.schedule("8,17,25,34,42,51,59 * * * *", function(){
     console.log('scheduler running...')
     if(shell.exec('node server.js').code!==0){
  console.log('Something went wrong ')
     }
 })
+app.get('/', function(req,res){
+    res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
+})
+app.listen(9000, () => console.log(`Server started on port on port 9000`));
