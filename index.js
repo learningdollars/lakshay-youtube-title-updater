dc321357fd612e6972a83e19feb0ed210f7a4cc8
lakshay <guptalakshay90@gmail.com>
2020-06-18 15:44:55 +0530

    Changed port

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ cron.schedule("8,17,25,34,42,51,59 * * * *", function(){
 app.get('/', function(req,res){
     res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
 })
-app.listen(9000, () => console.log(`Server started on port on port 9000`));
+app.listen(3000, () => console.log(`Server started on port on port 3000`));
