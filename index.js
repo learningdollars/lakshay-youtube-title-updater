0dc19bd782544a9f81ec7cd272dd563ff2bd1528
lakshay <guptalakshay90@gmail.com>
2020-06-18 15:48:21 +0530

    dsjbs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ cron.schedule("8,17,25,34,42,51,59 * * * *", function(){
  console.log('Something went wrong ')
     }
 })
-app.get('/', function(req,res){
+/*app.get('/', function(req,res){
     res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
-})
+})*/
 app.listen(3000, () => console.log(`Server started on port on port 3000`));
