c145929baa4e96b9416db433ae7a491ba2b78e03
lakshay <guptalakshay90@gmail.com>
2020-06-18 15:51:28 +0530

    jfbdsc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let cron = require('node-cron')
 let shell = require('shelljs')
 var express = require('express')
 var app = express()
-cron.schedule("* * * * * *", function(){
+cron.schedule("8,17,25,34,42,51,59 * * * *", function(){
     console.log('scheduler running...')
     if(shell.exec('node server.js').code!==0){
  console.log('Something went wrong ')
