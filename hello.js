1821c2b7fc2e906d51f10d62cb624b96aa990217
lakshay <guptalakshay90@gmail.com>
2020-06-18 16:17:43 +0530

    sdmhvhcsdc

---
+++ hello.js
@@ -0,0 +1,8 @@
+async function main() {
+    try {
+     console.log('Hello World')
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  main()
--- index.js
+++ index.js
@@ -2,13 +2,23 @@ let cron = require('node-cron')
 let shell = require('shelljs')
 var express = require('express')
 var app = express()
-cron.schedule("1,17,25,34,42,51,59 * * * *", function(){
+
+app.get('/', function(req,res){
+    res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
+}) 
+
+cron.schedule("8 17,25,34,42,51,59 * * * *", function(){
     console.log('scheduler running...')
     if(shell.exec('node server.js').code!==0){
  console.log('Something went wrong ')
     }
 })
-app.get('/', function(req,res){
-    res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
+
+cron.schedule("* * * * * *", function(){
+    console.log('hello scheduler running...')
+    if(shell.exec('node hello.js').code!==0){
+ console.log('Something went wrong ')
+    }
 })
-app.listen(3000, () => console.log(`Server started on port 3000`));
+
+app.listen(9000, () => console.log(`Server started on port 3000`));
