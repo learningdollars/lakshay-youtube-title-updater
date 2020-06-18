17fea9b9b87e3566b4f7b7502dc42b4865876827
lakshay <guptalakshay90@gmail.com>
2020-06-18 16:24:59 +0530

    s,ajbds

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req,res){
-    res.send('Head over to https://www.youtube.com/watch?v=wYOA4waoWog to check the video')
+    res.redirect("https://www.youtube.com/watch?v=wYOA4waoWog")
 }) 
 
 cron.schedule("8 17,25,34,42,51,59 * * * *", function(){
@@ -13,12 +13,12 @@ cron.schedule("8 17,25,34,42,51,59 * * * *", function(){
  console.log('Something went wrong ')
     }
 })
-
+/*
 cron.schedule("* * * * * *", function(){
     console.log('hello scheduler running...')
     if(shell.exec('node hello.js').code!==0){
  console.log('Something went wrong ')
     }
 })
-
+*/
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port 3000`));
