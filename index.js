7b419aa7438438eb17a610f15cd62d8122713aa0
lakshay <guptalakshay90@gmail.com>
2020-06-18 16:21:28 +0530

    sdjvdsvcs

--- index.js
+++ index.js
@@ -21,4 +21,4 @@ cron.schedule("* * * * * *", function(){
     }
 })
 
-app.listen(9000, () => console.log(`Server started on port 3000`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server started on port 3000`));
