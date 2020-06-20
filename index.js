1811e434b15b3ddffc1825e05c3d68ffaab738be
lakshay <guptalakshay90@gmail.com>
2020-06-20 11:24:51 +0530

    change

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ app.get('/', function(req,res){
     res.redirect("https://www.youtube.com/watch?v=wYOA4waoWog")
 }) 
 
-cron.schedule("8 17,25,34,42,51,59 * * * *", function(){
+cron.schedule("* * * * * *", function(){
     console.log('scheduler running...')
     if(shell.exec('node server.js').code!==0){
  console.log('Something went wrong ')
     }
 })
-/*
-cron.schedule("* * * * * *", function(){
-    console.log('hello scheduler running...')
-    if(shell.exec('node hello.js').code!==0){
- console.log('Something went wrong ')
-    }
-})
-*/
+
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port 3000`));
